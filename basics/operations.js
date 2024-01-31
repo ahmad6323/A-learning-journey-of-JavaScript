cc6ce48e913bb69143a97a4afd6c9a03b35d5597
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-01-31 05:20:49 +0000

    memory

--- basics/operations.js
+++ basics/operations.js
@@ -19,3 +19,28 @@ console.log(1 + 2 + "2")
 let counter = 100 
 counter++
 console.log(counter)
+
+// some memory concepts in javascript
+//stack and heap 
+//stack are primitive means gives you the value the variable 
+ let myname = "ahmad"
+ let anothername = myname
+ console.log(anothername)
+ anothername = "muhammad"
+ console.log(anothername)
+ console.log(myname)
+
+ // this example shows that you are changing in the copy the variable not in the original value
+
+
+// heap is a non primitive means it gives you a reference of the value or variable 
+ let user1 = {
+    email:"ak@gmail.com"
+ }
+ let user2 = user1
+ user2.email = "ka@gmail.com"
+ console.log(user1.email)
+ console.log(user2.email)
+ 
+
+ // so this heap example shows that you are changing in the orginal value
