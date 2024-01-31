11f0532506c160a3171e3361a901c0a13cb89364
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-01-31 04:13:24 +0000

    conversion

---
+++ basics/converstions.js
@@ -0,0 +1,17 @@
+let score  = "33abc"
+console.log(typeof score)
+let isNumber =  Number(score)
+console.log(isNumber)
+console.log(typeof isNumber)
+
+
+// nan => not a number
+// " "=> false
+// "number" > ture in case of boolen datatype 
+
+let number  = 33
+console.log(number)
+console.log(typeof number)
+let stringForm  = String(number)
+console.log(stringForm)
+console.log(typeof stringForm)
