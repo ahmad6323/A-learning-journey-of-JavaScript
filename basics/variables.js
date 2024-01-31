ad93a2d7e2d846320a06c56f6ea6a9f1caedf17a
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-01-31 03:45:41 +0000

    first commit

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const test = "ahmad"
+let test1  = "ali"
+var test2 = 112233
+test3 = "js"
+
+//now assigning the values 
+test1 = "muhammad"
+test2 = 44333
+test3 = "javascript"
+
+console.table([test1, test2, test3])
+// NOW IN modern js we are not using the var keyword because of 
+// conditional statements we must perfer let instead of var 
