6c97eed56d1fb6ac4244936314a2924722ffcc06
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-01-31 04:00:56 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,20 @@
+// "use strict " treat all js code as newer version
+let name = "ahmad"
+let number  = 22
+let bool = true
+
+console.table([name, number, bool])
+// number 2=> to power 53
+// bigint
+/*
+  boolen => true or false
+  null  is a stand alone value
+  string ""
+  undefined   // where value is not assigned
+  symbol   => unique
+  objects
+  
+
+
+
+*/
