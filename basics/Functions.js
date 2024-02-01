d28dc2182b43fe56f1ad4cdd470d52e365475ebd
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-02-01 14:42:57 +0000

    changes

--- basics/Functions.js
+++ basics/Functions.js
@@ -21,3 +21,33 @@ function handleObject (anyobj){
    console.log(`the name of the user is ${anyobj.name} and the age of the user is ${anyobj.age}`)
 }
 handleObject(obj)
+
+// arrow functions 
+
+let add = (num1, num2) =>{
+  return num1 + num2
+}
+add(22,33)
+
+// another method of declaration
+  let add1 = (num3, num4) => num3 + num4
+
+  // another 
+
+  let add2 = (num5, num6) => (num5 + num6)
+
+  
+
+// this keyword tell us the current context 
+let object  = {
+    name: 'ahmad',
+    helloMessage: function (){
+        console.log(`${this.name}  wellcome our website `)
+    }
+    
+}
+object.helloMessage()
+object.name = "ali"
+object.helloMessage()
+console.log(this)
+
--- basics/variables.js
+++ basics/variables.js
@@ -11,3 +11,30 @@ test3 = "javascript"
 console.table([test1, test2, test3])
 // NOW IN modern js we are not using the var keyword because of 
 // conditional statements we must perfer let instead of var 
+
+// now here disscussing the global and local variable 
+
+let a = 300;
+if(true){
+   let a = 20;
+    console.log('inner block variable ', a)
+}
+
+console.log(a)
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+console.log(addto(5)) 
+ const addto = function addtwo(num){
+    return num + 2
+}
+
+
+
+
