1bd587852a6e3dfecf13351786d97596d24c6c97
AHMAD <111060767+ahmad6323@users.noreply.github.com>
2024-02-04 08:43:27 +0000

    loop

--- advanced/loops.js
+++ advanced/loops.js
@@ -1,4 +1,15 @@
-// ofr loops 
+// for loops 
 for(let i = 0 ; i<=10; i++){
     console.log(i)
 }
+
+// foreach loop
+const coding   = ['js','ruby','java','css','cpp']
+coding.forEach((item)=>{
+    console.log(item)
+})
+
+
+const num = [1,2,3,4,5,6,7,89,9]
+const values  =  num.filter((num)=>(num))
+console.log(values)
